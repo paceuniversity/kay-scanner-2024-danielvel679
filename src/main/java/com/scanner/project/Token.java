e32a2da6057ee08ad367ae5ef6b6ea12632cd4d2
Daniel Velez <danielvel679@gmail.com>
2025-11-24 16:37:12 -0500

    Update Token.java

--- src/main/java/com/scanner/project/Token.java
+++ src/main/java/com/scanner/project/Token.java
@@ -8,31 +8,35 @@ public class Token {
     // Token value
     private String value;
 
-    
+    /*
      
 @param value
 . Set the value of a Token.*/
 public void setValue(String value) {
-this.value = value;}
+this.value = value;
+}
 
-    
+    /*
      
 @return Returns the value of a Token.*/
   public String getValue() {
-      return value;}
+      return value;
+  }
 
-    
+    /*
      
 @param type
 . Set the type of a Token.*/
 public void setType(String type) {
-this.type = type;}
+this.type = type;
+}
 
-    
+    /*
      
 @return Returns the type of a Token.*/
   public String getType() {
-      return type;}
+      return type;
+  }
 
     public String toString() {
         return "Value: " + this.getValue() + " " + "Type: " + this.getType();
